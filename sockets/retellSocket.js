1dd82f90991785af5f34edd9db97ce6938caf8a5
nexella-ai <nexellaai@gmail.com>
2025-05-30 15:57:46 -0700

    Delete sockets directory

--- sockets/retellSocket.js
+++
@@ -1,8 +0,0 @@
-const { handleWebSocketConnection } = require('./retellSocketHandler');
-
-function setupSocket(wss) {
-  wss.on('connection', handleWebSocketConnection);
-  wss.on('error', (err) => console.error('WebSocket error:', err));
-}
-
-module.exports = { setupSocket };
